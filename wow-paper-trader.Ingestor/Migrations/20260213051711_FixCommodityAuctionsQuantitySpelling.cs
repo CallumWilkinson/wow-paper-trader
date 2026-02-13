eb543239b6711324c564e0658fe0fe680060d4ad
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-02-13 16:22:49 +1100

    fix(migrations) redo Quantity column rename fix

---
+++ wow-paper-trader.Ingestor/Migrations/20260213051711_FixCommodityAuctionsQuantitySpelling.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wow_paper_trader.Ingestor.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCommodityAuctionsQuantitySpelling : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quanity",
+                table: "CommodityAuctions",
+                newName: "Quantity");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quantity",
+                table: "CommodityAuctions",
+                newName: "Quanity");
+        }
+    }
+}
