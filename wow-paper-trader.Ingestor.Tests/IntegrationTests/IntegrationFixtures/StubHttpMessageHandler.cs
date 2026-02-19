a8f3c3d500e2887608300d6bf92a935d135c7fd2
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-02-19 16:39:41 +1100

    feat(test) add StubHttpMessageHandler fixture for mocks - allows us to integration test our custom http clients by intercepting the http sendAsync function and return our custom test json instead of hitting the actual blizzard api - can also pass any response code so we can use this fixture for testing failed http requests

---
+++ wow-paper-trader.Ingestor.Tests/IntegrationTests/IntegrationFixtures/StubHttpMessageHandler.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using System.Text;
+
+public sealed class StubHttpMessageHandler : HttpMessageHandler
+{
+    private readonly string _customJsonResponse;
+
+    private readonly HttpStatusCode _statusCode;
+
+    public StubHttpMessageHandler(string customJsonResponse, HttpStatusCode statusCode)
+    {
+        _customJsonResponse = customJsonResponse;
+        _statusCode = statusCode;
+
+    }
+
+    //override the transport layer so we never send data over TCP, we just intercept the "http send" to return our custom json response
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var httpResponse = new HttpResponseMessage
+        {
+            StatusCode = _statusCode,
+            Content = new StringContent(
+                _customJsonResponse,
+                Encoding.UTF8,
+                //set content-type header
+                "application/json"
+            )
+        };
+
+        return Task.FromResult(httpResponse);
+
+    }
+}
