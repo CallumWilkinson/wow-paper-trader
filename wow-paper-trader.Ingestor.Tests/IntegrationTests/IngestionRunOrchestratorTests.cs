0dfe71f299c2c865b15c167fc69f0af66f0332dc
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-02-22 08:33:11 +1100

    test: add asserts to ingestionRunOrchestratorTests

--- wow-paper-trader.Ingestor.Tests/IntegrationTests/IngestionRunOrchestratorTests.cs
+++ wow-paper-trader.Ingestor.Tests/IntegrationTests/IngestionRunOrchestratorTests.cs
@@ -1,14 +1,18 @@
 using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using wow_paper_trader.Ingestor;
 
-public sealed class DataIngestorBackgroundServiceTests : IClassFixture<SqliteInMemoryDbFixture>
+namespace wow_paper_trader.Ingestor.Tests;
+
+
+public sealed class IngestionRunOrchestratorTests : IClassFixture<SqliteInMemoryDbFixture>
 {
     private readonly SqliteInMemoryDbFixture _db;
 
-    public DataIngestorBackgroundServiceTests(SqliteInMemoryDbFixture db)
+    public IngestionRunOrchestratorTests(SqliteInMemoryDbFixture db)
     {
         _db = db;
     }
@@ -37,18 +41,28 @@ public sealed class DataIngestorBackgroundServiceTests : IClassFixture<SqliteInM
         }
         """;
 
-        await using var dbContext = _db.CreateDbContext();
+        await using var arrangeDbContext = _db.CreateDbContext();
 
         var testBuilder = new TestBuilderForIngestionRunOrchestrator(authJson, auctionsJson);
-        var ingestionRunOrchestrator = testBuilder.CreateOrchestrator(dbContext);
+        var ingestionRunOrchestrator = testBuilder.CreateOrchestrator(arrangeDbContext);
 
         //Act   
         await ingestionRunOrchestrator.RunOnceAsync(CancellationToken.None);
 
         //Assert
-        //use 1 assertdB per test (not per assertion)
-        await using var assertDb = _db.CreateDbContext();
+        await using var assertDbContext = _db.CreateDbContext();
 
-        //assert on the sql in memory db
+        int ingestionRunRowCount = await assertDbContext.IngestionRuns.CountAsync();
+        int snapshotRowCount = await assertDbContext.CommodityAuctionSnapshots.CountAsync();
+        int auctionRowCount = await assertDbContext.CommodityAuctions.CountAsync();
+        IngestionRun ingestionRunRow = await assertDbContext.IngestionRuns.SingleAsync(x => x.Id == 1);
+
+        Assert.Equal(1, ingestionRunRowCount);
+
+        Assert.Equal(IngestionRunStatus.Finished, ingestionRunRow.Status);
+
+        // Assert.Equal(1, snapshotRowCount);
+        // Assert.Equal(2, auctionRowCount);
+
     }
 }
