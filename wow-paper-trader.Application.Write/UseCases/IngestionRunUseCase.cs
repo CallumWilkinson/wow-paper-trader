0f496d2fa3a72bbaa6b7806199d7b83b45cb3d12
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-07 14:28:33 +1100

    refactor: add IngestionRunUseCase in app Write - defines behaviour for ingesion run - using Interfaces for api adaper and repo allow me to easily unit test without mocks - much cleaner and easier to reason about now taht it just makes an ingestion run, calls the api and saves the data to the db on a high level - this is now my buisness logic completely seperated out

--- wow-paper-trader.Application.Write/UseCases/IngestionRunUseCase.cs
+++ wow-paper-trader.Application.Write/UseCases/IngestionRunUseCase.cs
@@ -1,170 +1,40 @@
 using Microsoft.Extensions.Logging;
 
-//should be called IngestionRunUseCase
-//background service calls this UseCase once every hour
-public sealed class IngestionRunOrchestrator
+public sealed class IngestionRunUseCase
 {
-    private readonly ILogger<IngestionRunOrchestrator> _logger;
-    private readonly IngestorDbContext _dbContext;
+    private readonly ILogger<IngestionRunUseCase> _logger;
+    private readonly ICommodityAuctionApiAdapter _auctionApiAdapter;
+    private readonly ICommodityAuctionRepository _repository;
 
-    private readonly BattleNetAuthClient _authClient;
-
-    private readonly CommodityAuctionClient _CommodityAuctionClient;
-
-    public IngestionRunOrchestrator(
-    ILogger<IngestionRunOrchestrator> logger,
-    IngestorDbContext dbContext,
-    BattleNetAuthClient authClient,
-    CommodityAuctionClient CommodityAuctionClient)
+    public IngestionRunUseCase(
+        ILogger<IngestionRunUseCase> logger,
+        ICommodityAuctionApiAdapter auctionApiAdapter,
+        ICommodityAuctionRepository repository)
     {
         _logger = logger;
-        _dbContext = dbContext;
-        _authClient = authClient;
-        _CommodityAuctionClient = CommodityAuctionClient;
+        _auctionApiAdapter = auctionApiAdapter;
+        _repository = repository;
     }
 
     public async Task RunOnceAsync(CancellationToken cancellationToken)
     {
-        CancellationTokenRegistration cancellationRegistration =
-            cancellationToken.Register(() =>
-            {
-                Console.Error.WriteLine(
-                    "You have cancelled the program in the middle of an Ingestion Run. " +
-                    "You may have to wait up to 10 minutes for graceful shutdown."
-                );
-            });
-
-        var run = new IngestionRun();
-        _dbContext.IngestionRuns.Add(run);
-        await _dbContext.SaveChangesAsync(cancellationToken);
+        var run = await _repository.CreateIngestionRunAsync(cancellationToken);
 
         try
         {
-            await RunCommodityAuctionSnapshotDatabaseTransaction(run, cancellationToken);
-            _logger.LogInformation("All Auctions have been recorded in the database successfully!");
+            var auctionSnapshot = await _auctionApiAdapter.GetCommodityAuctionsSnapshotAsync(cancellationToken);
+
+            await _repository.SaveSnapshotAsync(run, auctionSnapshot, cancellationToken);
+
+            _logger.LogInformation("Ingestion completed successfully.");
         }
         catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested)
         {
-            return;
+            await _repository.MarkRunCancelledAsync(run);
         }
-
         catch (Exception ex)
         {
-            _dbContext.IngestionRuns.Attach(run);
-            run.MarkFailed(ex, DateTime.UtcNow);
-
-            await _dbContext.SaveChangesAsync(CancellationToken.None);
-            _logger.LogError(ex, "Ingestion run failed. RunId={RunId}", run.Id);
-
-            return;
-        }
-        finally
-        {
-            if (cancellationToken.IsCancellationRequested)
-            {
-                _dbContext.IngestionRuns.Attach(run);
-                run.TransitionTo(IngestionRunStatus.Cancelled, DateTime.UtcNow);
-
-                await _dbContext.SaveChangesAsync(CancellationToken.None);
-                _logger.LogError("Ingestion run cancelled. RunId={RunId}", run.Id);
-            }
-            _logger.LogInformation("Inserted IngestionRun row at {Time}", DateTimeOffset.Now);
-
-            cancellationRegistration.Dispose();
-        }
-
-    }
-
-
-    private async Task RunCommodityAuctionSnapshotDatabaseTransaction(IngestionRun run, CancellationToken cancellationToken)
-    {
-        await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
-
-        try
-        {
-            var tokenRequestedAt = DateTime.UtcNow;
-            run.TransitionTo(IngestionRunStatus.TokenRequested, tokenRequestedAt);
-
-            string? accessToken = await _authClient.RequestNewTokenAsync(cancellationToken);
-
-            if (accessToken == null)
-            {
-                throw new InvalidOperationException("Access token is null. OAuth token acquisition likely failed.");
-            }
-
-            CommodityAuctionClient.WowApiResult<CommodityAuctionsResponseDto> apiResult = await _CommodityAuctionClient.GetCommodityAuctionsAsync(accessToken, cancellationToken);
-
-            int auctionsCount = apiResult.Payload.CommodityAuctions.Count;
-            _logger.LogInformation("Total Auctions Received: {Count}", auctionsCount);
-
-
-            var mapper = new CommodityAuctionSnapshotMapper();
-            CommodityAuctionSnapshotResult snapshotEntity = mapper.MapToEntityFromDto(apiResult.Payload, run.Id, apiResult.DataReturnedAtUtc, apiResult.Endpoint);
-
-            var startingAdd = DateTime.UtcNow;
-            _logger.LogInformation("Adding to DbContext at {Time}", startingAdd);
-            _dbContext.CommodityAuctionSnapshots.Add(snapshotEntity);
-            _logger.LogInformation("DbContext Add took {Seconds} Seconds", (DateTime.UtcNow - startingAdd).TotalSeconds);
-
-            run.TransitionTo(IngestionRunStatus.Finished, apiResult.DataReturnedAtUtc);
-
-            var startingSaveToDb = DateTime.UtcNow;
-            _logger.LogInformation("Starting SQL Write at {Time}", startingSaveToDb);
-            await _dbContext.SaveChangesAsync(cancellationToken);
-            _logger.LogInformation("SQL Write took {Seconds} Seconds", (DateTime.UtcNow - startingSaveToDb).TotalSeconds);
-
-            await transaction.CommitAsync(cancellationToken);
-
-        }
-        catch (Exception)
-        {
-            await transaction.RollbackAsync(CancellationToken.None);
-            _dbContext.ChangeTracker.Clear();
-
-            //throw back up to caller's try catch
-            throw;
+            await _repository.MarkRunFailedAsync(run, ex);
         }
     }
 }
-
-
-
-// public sealed class IngestionRunOrchestrator
-// {
-//     private readonly ILogger<IngestionRunOrchestrator> _logger;
-//     private readonly IAuctionApi _auctionApi;
-//     private readonly IAuctionRepository _repository;
-
-//     public IngestionRunOrchestrator(
-//         ILogger<IngestionRunOrchestrator> logger,
-//         IAuctionApi auctionApi,
-//         IAuctionRepository repository)
-//     {
-//         _logger = logger;
-//         _auctionApi = auctionApi;
-//         _repository = repository;
-//     }
-
-//     public async Task RunOnceAsync(CancellationToken cancellationToken)
-//     {
-//         IngestionRun run = await _repository.CreateRunAsync(cancellationToken);
-
-//         try
-//         {
-//             AuctionSnapshot snapshot =
-//                 await _auctionApi.GetCommoditySnapshotAsync(cancellationToken);
-
-//             await _repository.SaveSnapshotAsync(run, snapshot, cancellationToken);
-
-//             _logger.LogInformation("Ingestion completed successfully.");
-//         }
-//         catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested)
-//         {
-//             await _repository.MarkRunCancelledAsync(run);
-//         }
-//         catch (Exception ex)
-//         {
-//             await _repository.MarkRunFailedAsync(run, ex);
-//         }
-//     }
-// }
