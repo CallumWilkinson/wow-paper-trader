97e88e343d94658118fb9930e6a037b16409ff34
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-09 14:10:53 +1100

    refactor: wirte DI for DataIngestorBackgroundService - add use case and its dependencies, apiadapter and repo implementations - add a guard on base adress

--- wow-paper-trader.Ingestor/Program.cs
+++ wow-paper-trader.Ingestor/Program.cs
@@ -1,20 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using wow_paper_trader.Ingestor;
 using System.Net;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 
 var builder = Host.CreateApplicationBuilder(args);
 
-builder.Services.AddDbContext<IngestorDbContext>(options =>
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("WowPaperTrader");
     options.UseSqlServer(connectionString);
 });
 
-builder.Services.AddScoped<IngestionRunOrchestrator>();
-builder.Services.AddHostedService<DataIngestorBackgroundService>();
+builder.Services.AddHostedService<IngestionRunBackgroundService>();
+
+builder.Services.AddScoped<IngestionRunUseCase>();
+builder.Services.AddScoped<ICommodityAuctionApiAdapter, CommodityAuctionApiAdapter>();
+builder.Services.AddScoped<ICommodityAuctionRepository, CommodityAuctionRepository>();
 
 builder.Services.AddHttpClient<BattleNetAuthClient>()
     .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
@@ -23,9 +24,12 @@ builder.Services.AddHttpClient<BattleNetAuthClient>()
         PooledConnectionLifetime = TimeSpan.FromMinutes(10),
     });
 
+var wowApiBaseUrl = builder.Configuration["WowApi:BaseUrl"]
+?? throw new InvalidOperationException("WowApi:BaseUrl is missing.");
+
 builder.Services.AddHttpClient<CommodityAuctionClient>(client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration["WowApi:BaseUrl"]!);
+    client.BaseAddress = new Uri(wowApiBaseUrl);
 })
     .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
     {
