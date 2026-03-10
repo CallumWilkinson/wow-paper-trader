406614693a9d65ca0c67453dd41a65aa6ea8f619
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-10 18:13:28 +1100

    minor: change apiresult type to be clearer

--- wow-paper-trader.Application.Write/Contracts/WowApiResult.cs
+++ wow-paper-trader.Application.Write/Contracts/WowApiResult.cs
@@ -6,7 +6,7 @@ public sealed record WowApiResult<T>
 );
 
 public sealed record AuctionSnapshot(
-    IReadOnlyList<AuctionSnapshotRow> Auctions
+    List<AuctionSnapshotRow> Auctions
 );
 
 public sealed record AuctionSnapshotRow(
