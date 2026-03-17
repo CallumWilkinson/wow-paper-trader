e08b898cf8b2d0cebaf513ba4e12a2412936a8bb
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-17 18:29:27 +1100

    feat: setup swagger ui for testing end points

--- wow-paper-trader.Api.Read/Program.cs
+++ wow-paper-trader.Api.Read/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("WowPaperTrader");
+    options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddScoped<GetCurrentLowestUnitPriceByItemIdUseCase>();
 builder.Services.AddScoped<ICurrentLowestUnitPriceQuery, CurrentLowestUnitPriceQuery>();
@@ -12,10 +22,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.MapControllers();
 
