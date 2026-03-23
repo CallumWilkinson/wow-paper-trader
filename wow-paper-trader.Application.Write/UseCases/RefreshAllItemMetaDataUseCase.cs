6082a45b511c3e0a28fa67d8e0bfb46fc968fdb4
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-23 13:53:53 +1100

    feat: add try catch to metadata usecase

--- wow-paper-trader.Application.Write/UseCases/RefreshAllItemMetaDataUseCase.cs
+++ wow-paper-trader.Application.Write/UseCases/RefreshAllItemMetaDataUseCase.cs
@@ -1,34 +1,54 @@
+using Microsoft.Extensions.Logging;
+
 public sealed class RefreshAllItemMetaDataUseCase
 {
     private readonly ICommodityAuctionItemIdQuery _commodityAuctionItemIdQuery;
     private readonly IItemMetaDataApiAdapter _itemMetaDataApiAdapter;
     private readonly IItemMetaDataRepository _itemMetaDataRepository;
+    private readonly ILogger<RefreshAllItemMetaDataUseCase> _logger;
 
     public RefreshAllItemMetaDataUseCase(
         ICommodityAuctionItemIdQuery commodityAuctionItemIdQuery,
         IItemMetaDataApiAdapter itemMetaDataApiAdapter,
-        IItemMetaDataRepository itemMetaDataRepository
+        IItemMetaDataRepository itemMetaDataRepository,
+        ILogger<RefreshAllItemMetaDataUseCase> logger
     )
     {
         _commodityAuctionItemIdQuery = commodityAuctionItemIdQuery;
         _itemMetaDataApiAdapter = itemMetaDataApiAdapter;
         _itemMetaDataRepository = itemMetaDataRepository;
+        _logger = logger;
     }
 
     public async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        var itemIds = await _commodityAuctionItemIdQuery.GetAllUniqueItemIdsAsync(cancellationToken);
-
-        var itemMetaDataRecords = new List<ItemMetaDataRecord>();
-
-        foreach (long itemId in itemIds)
+        try
         {
-            var record = await _itemMetaDataApiAdapter.GetItemMetaDataAsync(itemId, cancellationToken);
+            var itemIds = await _commodityAuctionItemIdQuery.GetAllUniqueItemIdsAsync(cancellationToken);
 
-            itemMetaDataRecords.Add(record);
+            var itemMetaDataRecords = new List<ItemMetaDataRecord>();
+
+            foreach (long itemId in itemIds)
+            {
+                var record = await _itemMetaDataApiAdapter.GetItemMetaDataAsync(itemId, cancellationToken);
+
+                itemMetaDataRecords.Add(record);
+            }
+
+            await _itemMetaDataRepository.SaveItemMetaDataAsync(itemMetaDataRecords, cancellationToken);
+
+            _logger.LogInformation("Refresh All Item MetaData Use Case Completed Successfully");
+
+        }
+        catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Refresh All Item MetaData Use Case Cancelled");
+        }
+        catch
+        {
+            _logger.LogInformation("Refresh All Item MetaData Use Case Failed");
         }
 
-        await _itemMetaDataRepository.SaveItemMetaDataAsync(itemMetaDataRecords, cancellationToken);
     }
 
 }
