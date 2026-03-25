960876ececba9ea45bff1229a506fde6d36c7ed7
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-25 15:24:36 +1100

    feat: add ItemMetaDataClient implmentations - calls the actual api end point - sends http get request - returns dto

---
+++ wow-paper-trader.Infrastructure/HttpClients/ItemMetaDataClient.cs
@@ -0,0 +1,41 @@
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+public sealed class ItemMetaDataClient
+{
+    private readonly HttpClient _httpClient;
+
+    private static readonly JsonSerializerOptions _jsonOptions = new()
+    {
+        PropertyNameCaseInsensitive = true
+    };
+
+    public ItemMetaDataClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<ItemMetaDataResponseDto> GetAsync(string accessToken, long itemId, CancellationToken cancellationToken)
+    {
+        string endpointSuffix = $"item/{itemId}?namespace=static-us&locale=en_US";
+
+        using var request = new HttpRequestMessage(HttpMethod.Get, endpointSuffix);
+
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+        using var response = await _httpClient.SendAsync(request, cancellationToken);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var body = await response.Content.ReadAsStringAsync(cancellationToken);
+            throw new HttpRequestException($"WoW API ItemMetaData Request Failed during HTTP call. Status={(int)response.StatusCode} {response.ReasonPhrase}. Body={body}");
+        }
+
+        await using var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+
+        var result = await JsonSerializer.DeserializeAsync<ItemMetaDataResponseDto>(stream, _jsonOptions, cancellationToken)
+            ?? throw new JsonException("Wow Api ItemMetaData response JSON descerialised to null.");
+
+        return result;
+    }
+}
