c517e4a168fce26dc38f639dbb014c9372a9795a
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-25 16:13:22 +1100

    feat: add ItemMetaDataRepository implmentation - maps records to Entities - adds to db set and saves to db - save changes lives outisde foreach loop to avoid slow multiple changes

---
+++ wow-paper-trader.Persistence/Repositories/ItemMetaDataRepository.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.Logging;
+
+public class ItemMetaDataRepository : IItemMetaDataRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly ILogger<ItemMetaDataRepository> _logger;
+    public ItemMetaDataRepository(ApplicationDbContext dbContext, ILogger<ItemMetaDataRepository> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+    public async Task SaveItemMetaDataAsync(List<ItemMetaDataRecord> itemMetaDataRecords, CancellationToken cancellationToken)
+    {
+        var startingAdd = DateTime.UtcNow;
+
+        _logger.LogInformation("Adding to DbContext at {Time}", startingAdd);
+
+        foreach (ItemMetaDataRecord record in itemMetaDataRecords)
+        {
+            var itemMetaDataEntity = ItemMetaDataMapper.MapToEntity(record);
+            _dbContext.ItemMetaData.Add(itemMetaDataEntity);
+        }
+
+        _logger.LogInformation("DbContext Add took {Seconds} Seconds", (DateTime.UtcNow - startingAdd).TotalSeconds);
+
+        var startingSaveToDb = DateTime.UtcNow;
+
+        _logger.LogInformation("Starting SQL Write at {Time}", startingSaveToDb);
+
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        _logger.LogInformation("SQL Write took {Seconds} Seconds", (DateTime.UtcNow - startingSaveToDb).TotalSeconds);
+    }
+}
