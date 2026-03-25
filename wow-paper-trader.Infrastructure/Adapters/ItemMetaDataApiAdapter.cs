29c74abc822addbc6d02d9bd0eca8a00582db03a
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-25 15:22:06 +1100

    feat: add ItemMetaDataApiAdapter implementation in Infrastructure proj - implements the api call from write side - gets an auth token from blizzard and passes it through to the ItemMetaDataClient to get json - json deserialises to POCO dto using attributes to map exact response shape - mapper maps from DTO to Contract so write application only sees what it needs

---
+++ wow-paper-trader.Infrastructure/Adapters/ItemMetaDataApiAdapter.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Logging;
+
+public sealed class ItemMetaDataApiAdapter : IItemMetaDataApiAdapter
+{
+    private readonly BattleNetAuthClient _authClient;
+    private readonly ItemMetaDataClient _itemMetaDataClient;
+    private readonly ILogger<ItemMetaDataApiAdapter> _logger;
+
+    public ItemMetaDataApiAdapter(BattleNetAuthClient authClient, ItemMetaDataClient itemMetaDataClient, ILogger<ItemMetaDataApiAdapter> logger)
+    {
+        _authClient = authClient;
+        _itemMetaDataClient = itemMetaDataClient;
+        _logger = logger;
+    }
+    public async Task<ItemMetaDataRecord> GetItemMetaDataAsync(long itemId, CancellationToken cancellationToken)
+    {
+        string? accessToken = await _authClient.RequestNewTokenAsync(cancellationToken);
+
+        if (accessToken == null)
+        {
+            throw new InvalidOperationException("Access token is null. OAuth token acquisition likely failed.");
+        }
+
+        var dto = await _itemMetaDataClient.GetAsync(accessToken, itemId, cancellationToken);
+
+        var dataFetchedAtUtc = DateTime.UtcNow;
+
+        var contract = ItemMetaDataRecordMapper.MapToContract(dto, dataFetchedAtUtc);
+
+        return contract;
+
+    }
+}
