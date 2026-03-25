8ee143a76e107943a2c9b61bd1f922ea49197d6e
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-25 15:19:39 +1100

    feat: add write side contract to match entity shape - this defines what the api should return after the dto maps to it - this should match the entity and evenutally tables - this is the data that the front end will most likely care about - removes the confusing nested json from api

--- wow-paper-trader.Application.Write/Contracts/ItemMetaDataRecord.cs
+++ wow-paper-trader.Application.Write/Contracts/ItemMetaDataRecord.cs
@@ -1,26 +1,27 @@
 public sealed record ItemMetaDataRecord
 {
-    long ItemId;
-    string Name;
-    string SelfHref;
-    string? QualityType;
-    string? QualityName;
-    int Level;
-    int RequiredLevel;
-    long? MediaId;
-    string? MediaHref;
-    int? ItemClassId;
-    string? ItemClassName;
-    int? ItemSubclassId;
-    string? ItemSubclassName;
-    string? InventoryType;
-    string? InventoryTypeName;
-    long? PurchasePrice;
-    long? SellPrice;
-    int? MaxCount;
-    bool IsEquippable;
-    bool IsStackable;
-    int? PurchaseQuantity;
-    string RawJson;
-    DateTime LastFetchedUtc;
+    public long ItemId { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string? QualityType { get; init; }
+    public string? QualityName { get; init; }
+    public int Level { get; init; }
+    public int RequiredLevel { get; init; }
+    public int? ItemClassId { get; init; }
+    public string? ItemClassName { get; init; }
+    public int? ItemSubclassId { get; init; }
+    public string? ItemSubclassName { get; init; }
+    public int? ProfessionId { get; init; }
+    public string? ProfessionName { get; init; }
+    public int? ProfessionSkillLevel { get; init; }
+    public string? SkillDisplayString { get; init; }
+    public string? CraftingReagent { get; init; }
+    public string? InventoryType { get; init; }
+    public string? InventoryTypeName { get; init; }
+    public long? PurchasePrice { get; init; }
+    public long? SellPrice { get; init; }
+    public int? MaxCount { get; init; }
+    public bool IsEquippable { get; init; }
+    public bool IsStackable { get; init; }
+    public int? PurchaseQuantity { get; init; }
+    public DateTime LastFetchedUtc { get; init; }
 }
