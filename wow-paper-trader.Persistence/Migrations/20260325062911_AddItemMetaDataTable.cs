5c9687d3625ea977f9de4d4953ec7da2f13c5699
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-25 19:44:31 +1100

    fix: add new db migraitons for AddItemMetaDataTable

---
+++ wow-paper-trader.Persistence/Migrations/20260325062911_AddItemMetaDataTable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wow_paper_trader.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItemMetaDataTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ItemMetaData",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemId = table.Column<long>(type: "bigint", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QualityType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QualityName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    RequiredLevel = table.Column<int>(type: "int", nullable: false),
+                    ItemClassId = table.Column<int>(type: "int", nullable: true),
+                    ItemClassName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ItemSubclassId = table.Column<int>(type: "int", nullable: true),
+                    ItemSubclassName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProfessionId = table.Column<int>(type: "int", nullable: true),
+                    ProfessionName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProfessionSkillLevel = table.Column<int>(type: "int", nullable: true),
+                    SkillDisplayString = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CraftingReagent = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InventoryType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InventoryTypeName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PurchasePrice = table.Column<long>(type: "bigint", nullable: true),
+                    SellPrice = table.Column<long>(type: "bigint", nullable: true),
+                    MaxCount = table.Column<int>(type: "int", nullable: true),
+                    IsEquippable = table.Column<bool>(type: "bit", nullable: false),
+                    IsStackable = table.Column<bool>(type: "bit", nullable: false),
+                    PurchaseQuantity = table.Column<int>(type: "int", nullable: true),
+                    LastFetchedUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemMetaData", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemMetaData");
+        }
+    }
+}
