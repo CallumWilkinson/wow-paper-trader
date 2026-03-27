0d46ced69d3fddeef0bd3ecb7222c77a456319d8
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-27 13:11:01 +1100

    test: CommodityAuctionItemIdQueryTests add green test - uses fresh db with 3 auctions and expects to get back the 2 unique item ids

---
+++ wow-paper-trader.Persistence.Tests/IntegrationTests/QueryTests/CommodityAuctionItemIdQueryTests.cs
@@ -0,0 +1,35 @@
+using FluentAssertions;
+
+public sealed class CommodityAuctionItemIdQueryTests : IClassFixture<SqliteInMemoryDbFixture>
+{
+    private readonly SqliteInMemoryDbFixture _db;
+
+    public CommodityAuctionItemIdQueryTests(SqliteInMemoryDbFixture db)
+    {
+        _db = db;
+    }
+
+    [Fact]
+    public async Task GetAllUniqueItemIdsAsync_ShouldReturn_AllUniqueItemIds_ContainedIn_CommodityAuctionsTable()
+    {
+        //arrange
+        await using var arrangeDbContext = await _db.CreateArrangeDbContextAsync();
+
+        var snapshotTime = new DateTime(2026, 3, 12, 9, 0, 0, DateTimeKind.Utc);
+
+        await CommodityAuctionSnapshotTestFactory.AddNewerSnapshotToDbAsync(arrangeDbContext, snapshotTime);
+
+        var query = new CommodityAuctionItemIdQuery(arrangeDbContext);
+
+        //act
+        List<long> result = await query.GetAllUniqueItemIdsAsync(CancellationToken.None);
+
+        //assert
+        await using var assertDbContext = _db.CreateAssertDbContext();
+
+        result.Should().NotBeNull();
+        result.Should().HaveCount(2);
+        result.Should().Contain(2770);
+        result.Should().Contain(9999);
+    }
+}
