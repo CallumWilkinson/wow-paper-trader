1ab3bea532bba4a4dc391dfbec8d8d04dafcd12a
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-30 15:30:36 +1100

    feat: add GetMetadataAndPriceByItemIdUseCase to app.Read - first phase of implementing a new end point - allows us to get both lowest unit price and metadata for tooltip in one trip to the db - this will save us time and make the end react project must faster

---
+++ wow-paper-trader.Application.Read/UseCases/GetMetadataAndPriceByItemIdUseCase.cs
@@ -0,0 +1,26 @@
+public sealed class GetMetadataAndPriceByItemIdUseCase
+{
+    private readonly IItemMetadataAndPriceQuery _query;
+
+    public GetMetadataAndPriceByItemIdUseCase(IItemMetadataAndPriceQuery query)
+    {
+        _query = query;
+    }
+
+    public async Task<ItemMetadataAndPriceResult?> ExecuteAsync(
+        long itemId,
+        CancellationToken cancellationToken
+    )
+    {
+        if (itemId <= 0)
+        {
+            throw new ArgumentOutOfRangeException
+            (
+                nameof(itemId),
+                "Invalid itemId"
+            );
+        }
+
+        return await _query.GetAsync(itemId, cancellationToken);
+    }
+}
