b581337566b06bb5603e3d35063749bf89190e25
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-03-30 20:56:47 +1100

    migrations: add for new schema change to metadata table

---
+++ wow-paper-trader.Persistence/Migrations/20260330094450_AddImageUrlToItemMetaData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wow_paper_trader.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToItemMetaData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
