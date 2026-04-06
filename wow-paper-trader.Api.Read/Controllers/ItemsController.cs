00afd8e6fc18eb05391109de04fb9996733b0b12
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-04-06 14:45:46 +1000

    refactor: merge Controllers into one file to align with resource focused URL design - makes more sense to have controllers coupled into one file as they are all pointing to the one items resource - lowest price is a resource belonging to auctions, not to the item itself - theres nothing to an item resource other than its metadata so it doesnt need to specify metadata - URL is now much more RESTful and user focused

---
+++ wow-paper-trader.Api.Read/Controllers/ItemsController.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace wow_paper_trader.Api.Read.Controllers;
+
+
+[ApiController]
+[Route("api/v1/items")]
+public sealed class ItemsController : ControllerBase
+{
+    private readonly GetCurrentLowestUnitPriceByItemIdUseCase _lowestPriceUseCase;
+    private readonly ItemSearchUseCase _itemSearchUseCase;
+    private readonly GetMetadataAndPriceByItemIdUseCase _metadataUseCase;
+
+    public ItemsController(
+        GetCurrentLowestUnitPriceByItemIdUseCase lowestPriceUseCase,
+        ItemSearchUseCase itemSearchUseCase,
+        GetMetadataAndPriceByItemIdUseCase metadataUseCase)
+    {
+        _lowestPriceUseCase = lowestPriceUseCase;
+        _itemSearchUseCase = itemSearchUseCase;
+        _metadataUseCase = metadataUseCase;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<ItemSearchResult>>> SearchItems([FromQuery] string itemName, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrWhiteSpace(itemName))
+        {
+            return BadRequest("Name is required.");
+        }
+
+        var result = await _itemSearchUseCase.ExecuteAsync(itemName, cancellationToken);
+
+        return Ok(result);
+    }
+
+    [HttpGet("{itemId:long}")]
+    public async Task<ActionResult<ItemMetadataAndPriceResult>> GetTooltipAndLowestBuyoutPrice(
+    long itemId,
+    CancellationToken cancellationToken
+)
+    {
+        if (itemId <= 0)
+        {
+            return BadRequest("Invalid itemId.");
+        }
+
+        var result = await _metadataUseCase.ExecuteAsync(itemId, cancellationToken);
+
+        if (result is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpGet("{itemId:long}/auctions/lowest")]
+    public async Task<ActionResult<CurrentLowestUnitPriceResult>> GetCurrentLowestUnitPrice(
+    long itemId,
+    CancellationToken cancellationToken
+)
+    {
+        if (itemId <= 0)
+        {
+            return BadRequest("Invalid itemId.");
+        }
+
+        var result = await _lowestPriceUseCase.ExecuteAsync(itemId, cancellationToken);
+
+        if (result is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+
+}
