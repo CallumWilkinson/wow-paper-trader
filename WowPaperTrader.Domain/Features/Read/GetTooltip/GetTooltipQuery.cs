8fbb5fb0a47ecd824a2d19b5ce4cd022f29d5143
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-04-11 10:58:51 +1000

    feat: add outline for GetTooltipQuery.cs

---
+++ WowPaperTrader.Domain/Features/Read/GetTooltip/GetTooltipQuery.cs
@@ -0,0 +1,8 @@
+using WowPaperTrader.Domain.Architecture;
+
+namespace WowPaperTrader.Domain.Features.Read.GetTooltip;
+
+public class GetTooltipQuery : IQuery<ItemMetadataAndPriceResponse>
+{
+    
+}
