652a15357065772719dc2639a63cb23c9a54da8d
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-04-14 17:22:06 +1000

    feat: add item search types matching backend response types

---
+++ WowPaperTrader.Frontend/src/Features/ItemSearch/Types/ItemSearchTypes.ts
@@ -0,0 +1,48 @@
+export interface ItemSearchResponse {
+  itemId: number;
+  name: string;
+}
+
+export interface ItemMetadataResponse {
+  itemId: number;
+  unitPrice: number;
+  priceTakenAtUtc: string;
+
+  name: string;
+
+  qualityType: string | null;
+  qualityName: string | null;
+
+  level: number;
+  requiredLevel: number;
+
+  itemClassId: number | null;
+  itemClassName: string | null;
+
+  itemSubclassId: number | null;
+  itemSubclassName: string | null;
+
+  professionId: number | null;
+  professionName: string | null;
+  professionSkillLevel: number | null;
+
+  skillDisplayString: string | null;
+  craftingReagent: string | null;
+
+  inventoryType: string | null;
+  inventoryTypeName: string | null;
+
+  purchasePrice: number | null;
+  sellPrice: number | null;
+
+  maxCount: number | null;
+
+  isEquippable: boolean;
+  isStackable: boolean;
+
+  purchaseQuantity: number | null;
+
+  imageUrl: string | null;
+
+  metadataLastFetchedUtc: string;
+}
