afe4ebadb9dc32a04b9d2ec7d26ea37fd5d1d207
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-04-14 17:07:26 +1000

    feat: add axios HttpClient setup

---
+++ WowPaperTrader.Frontend/src/AxiosApi/HttpClient.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const apiBaseUrl = import.meta.end.VITE_API_BASE_URL;
+
+if (!apiBaseUrl) {
+  throw new Error(
+    "VITE_API_BASE_URL is missing. Ensure .env file exists for HTTPClient setup",
+  );
+}
+
+export const httpClient = axios.create({
+  baseURL: apiBaseUrl,
+  timeout: 10_000,
+});
