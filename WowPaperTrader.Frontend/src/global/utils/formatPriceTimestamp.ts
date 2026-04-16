d8adb360cea085d5359d0a98685b7fe9a7967da2
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-04-16 15:25:02 +1000

    feat: add formatPriceTimestamp util function - allows price last update at to be displayed in an intuitive way - user can see clearly when the price was updated - allows me to signal that the app is using live data - this will work in conjunction with a hook i will make next that will allow it to be updated automatically every minute

---
+++ WowPaperTrader.Frontend/src/global/utils/formatPriceTimestamp.ts
@@ -0,0 +1,37 @@
+//TODO: write unit test for this
+export function formatPriceTimestamp(utcString: string): string {
+  const date = new Date(utcString);
+  const now = new Date();
+
+  const diffMs = now.getTime() - date.getTime();
+  const diffSeconds = Math.floor(diffMs / 1000);
+
+  let relative: string;
+
+  if (diffSeconds < 60) {
+    relative = "just now";
+  } else {
+    const diffMinutes = Math.floor(diffSeconds / 60);
+
+    if (diffMinutes < 60) {
+      relative = `${diffMinutes} minute${diffMinutes === 1 ? "" : "s"} ago`;
+    } else {
+      const diffHours = Math.floor(diffMinutes / 60);
+
+      if (diffHours < 24) {
+        relative = `${diffHours} hour${diffHours === 1 ? "" : "s"} ago`;
+      } else {
+        const diffDays = Math.floor(diffHours / 24);
+
+        relative = `${diffDays} day${diffDays === 1 ? "" : "s"} ago`;
+      }
+    }
+  }
+
+  const exact = date.toLocaleTimeString(undefined, {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return `Price updated ${relative} (${exact})`;
+}
