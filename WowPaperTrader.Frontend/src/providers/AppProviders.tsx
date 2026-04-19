e93fa7877102230173e677ba3db93229ba924ac8
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-04-19 18:39:32 +1000

    feat: add AppProviders to register dependencies - wraps around my app which contains the pages - allows the pages to use react query - creates MUI theme - add debug pannel for react query dev tools

---
+++ WowPaperTrader.Frontend/src/providers/AppProviders.tsx
@@ -0,0 +1,33 @@
+import { ReactNode, useState } from "react";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+interface AppProvidersProps {
+  children: ReactNode;
+}
+
+export default function AppProviders(props: AppProvidersProps) {
+  const { children } = props;
+
+  const [queryClient] = useState(() => {
+    return new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+        },
+      },
+    });
+  });
+
+  const theme = createTheme();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>{children}</CssBaseline>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
+    </QueryClientProvider>
+  );
+}
