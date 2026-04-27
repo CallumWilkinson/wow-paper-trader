99a341414fdd4164e9918caa6b304623919997c0
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-04-27 12:03:41 +1000

    fix(test): include ImgUrl in expected result for ItemSearchTest

--- WowPaperTrader.Persistence.Tests/TestHelpers/ItemSearchResultFactory.cs
+++ WowPaperTrader.Persistence.Tests/TestHelpers/ItemSearchResultFactory.cs
@@ -9,15 +9,15 @@ public static class ItemSearchResultFactory
         return new List<ItemSearchResponse>
         {
             // Exact match
-            new() { ItemId = 2589, Name = "Linen Cloth" },
+            new() { ItemId = 2589, Name = "Linen Cloth", ImageUrl = "fake/url" },
 
             // Starts with "linen"
-            new() { ItemId = 1251, Name = "Linen Bandage" },
+            new() { ItemId = 1251, Name = "Linen Bandage", ImageUrl = "fake/url" },
 
             // Contains "linen"
-            new() { ItemId = 10001, Name = "Heavy Linen Pants" },
-            new() { ItemId = 10002, Name = "Fine Linen Shirt" },
-            new() { ItemId = 10003, Name = "linen robe" }
+            new() { ItemId = 10001, Name = "Heavy Linen Pants", ImageUrl = "fake/url" },
+            new() { ItemId = 10002, Name = "Fine Linen Shirt",  ImageUrl = "fake/url" },
+            new() { ItemId = 10003, Name = "linen robe", ImageUrl = "fake/url"}
         };
     }
 }
