c3fae6be7e526ba66000f4a134940bcfd2a2a99e
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-04-28 12:36:29 +1000

    chore: setup Jest

---
+++ WowPaperTrader.Frontend/jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import("jest").Config} */
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+  },
+};
