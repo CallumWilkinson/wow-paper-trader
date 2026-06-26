a43b486c5bf1b68d594fcb6a3f9f56d5d38be56e
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2026-06-26 12:36:14 +1000

    refactor: update db test fixture from sqlServer to Postrgress - NpgsqlDataSource handles connection pooling as per postgress docs - no longer need the awkward connection string - using test containers and respawn to improve CI, this fixture now works on any machine - bookworm linux for test fixture as thats what i will use in production

--- WowPaperTrader.Persistence.Tests/TestFixtures/PostgreSqlTestDbFixture.cs
+++ WowPaperTrader.Persistence.Tests/TestFixtures/PostgreSqlTestDbFixture.cs
@@ -1,49 +1,69 @@
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using Respawn;
+using Testcontainers.PostgreSql;
 
 namespace WowPaperTrader.Persistence.Tests.TestFixtures;
 
 public sealed class PostgreSqlTestDbFixture : IAsyncLifetime
 {
-    private static readonly string ConnectionString =
-        Environment.GetEnvironmentVariable("WOW_PAPER_TRADER_TEST_DB")
-        ?? throw new InvalidOperationException(
-            "Environment variable 'WOW_PAPER_TRADER_TEST_DB' is not set. " +
-            "Set it before running integration tests."
-        );
-    // copy-paste this to set environment variable to a new local test db that doesn't interact with regular project db
-    // [System.Environment]::SetEnvironmentVariable( "WOW_PAPER_TRADER_TEST_DB", "Server=CallumPC\SQLEXPRESS;Database=WowPaperTrader_TestDb;Trusted_Connection=True;TrustServerCertificate=True;", "User" )
-
-    public DbContextOptions<ApplicationDbContext> Options { get; private set; } = default!;
+    private readonly PostgreSqlContainer _postgreSqlContainer =
+        new PostgreSqlBuilder("postgres:18-bookworm")
+            .WithDatabase("wowpapertrader_test")
+            .WithUsername("test_user")
+            .WithPassword("test_password")
+            .Build();
+    
+    private NpgsqlDataSource? _dataSource;
+    
+    private DbContextOptions<ApplicationDbContext>? _options;
+    
+    private Respawner? _respawner;
+    
+    //getters
+    private DbContextOptions<ApplicationDbContext> Options =>
+        _options ?? throw new InvalidOperationException("The test database fixture has not been initialized. Did you forget to use the fixture via IClassFixture?");
+    
+    private NpgsqlDataSource DataSource =>
+        _dataSource ?? throw new InvalidOperationException("The test database fixture has not been initialized. Did you forget to use the fixture via IClassFixture?");
+    
+    private Respawner DatabaseRespawner =>
+    _respawner ?? throw new InvalidOperationException("The test database fixture has not been initialized. Did you forget to use the fixture via IClassFixture?");
 
     public async Task InitializeAsync()
     {
-        Options = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseNpgsql(ConnectionString)
+        await _postgreSqlContainer.StartAsync();
+
+        _dataSource = NpgsqlDataSource.Create(_postgreSqlContainer.GetConnectionString());
+        
+        _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseNpgsql(DataSource)
             .Options;
 
-        await ResetDatabaseAsync();
+        await CreateDatabaseSchemaAsync();
+        await CreateRespawnerAsync();
     }
 
     public async Task DisposeAsync()
     {
-        if (Options == null) return;
-
-        await using var db = new ApplicationDbContext(Options);
-
-        await db.Database.EnsureDeletedAsync();
+        if (_dataSource is not null)
+        {
+            await _dataSource.DisposeAsync();
+        }
+        
+        await _postgreSqlContainer.DisposeAsync();
     }
 
     private async Task ResetDatabaseAsync()
     {
-        if (Options == null)
-            throw new InvalidOperationException(
-                "Fixture not initialized. Did you forget to use the fixture via IClassFixture?");
-
-        await using var db = new ApplicationDbContext(Options);
-
-        await db.Database.EnsureDeletedAsync();
-
-        await db.Database.EnsureCreatedAsync();
+        await using NpgsqlConnection connection = await DataSource.OpenConnectionAsync();
+        
+        await DatabaseRespawner.ResetAsync(connection);
+    }
+    
+    private ApplicationDbContext CreateDbContext()
+    {
+        return new ApplicationDbContext(Options);
     }
 
     public async Task<ApplicationDbContext> CreateArrangeDbContextAsync()
@@ -57,11 +77,27 @@ public sealed class PostgreSqlTestDbFixture : IAsyncLifetime
         return CreateDbContext();
     }
 
-    private ApplicationDbContext CreateDbContext()
+    private async Task CreateDatabaseSchemaAsync()
     {
-        if (Options == null)
-            throw new InvalidOperationException(
-                "Fixture not initialized. Did you forget to use the fixture via IClassFixture?");
-        return new ApplicationDbContext(Options);
+        await using ApplicationDbContext db = CreateDbContext();
+        
+        await db.Database.EnsureCreatedAsync();
+        
+    }
+
+    private async Task CreateRespawnerAsync()
+    {
+        await using NpgsqlConnection connection = await DataSource.OpenConnectionAsync();
+
+        _respawner = await Respawner.CreateAsync(
+            connection,
+            new RespawnerOptions
+            {
+                DbAdapter = DbAdapter.Postgres,
+                SchemasToInclude =
+                [
+                    "public"
+                ]
+            });
     }
 }
